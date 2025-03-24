b83d9786f8a01de2b7732f47145e9efa439d22d9
Yana <annetart1994@gmail.com>
2025-03-24 23:59:43 +0200

    feat: add styles to help section

--- hippotherapy/src/components/sections/help-section/help-section.tsx
+++ hippotherapy/src/components/sections/help-section/help-section.tsx
@@ -5,57 +5,56 @@ import disabilityImg from '../../../assets/svg/svg-disability.svg';
 import familyImg from '../../../assets/svg/svg-family.svg';
 import puzzleImg from '../../../assets/svg/svg-puzzle.svg';
 import tankImg from '../../../assets/svg/svg-tank.svg';
+import style from './help-section.module.scss';
 
 export const HelpSection = () => {
   const { t } = useTranslation();
   return (
-    <section className="min-h-[372px] flex justify-center bg-white py-15 text-black">
-      <div className="max-w-[1440px] flex flex-col gap-4">
-        <h2 className="font-rubik text-[40px] leading-[60px] tracking-[0] font-extrabold">
-          {t('WeHelp')}
-        </h2>
-        <div className="flex flex-wrap">
-          <figure className="w-[275px] h-[132px] flex flex-col justify-center items-center gap-4">
+    <section className={style.section}>
+      <div className={style.block}>
+        <h2 className={style.title}>{t('WeHelp')}</h2>
+        <div className={style.figureBlock}>
+          <figure className={style.figure}>
             <Image
               src={familyImg}
-              alt="insta"
+              alt="family"
               width={80}
               height={60}
               priority
               className="object-contain"
             />
-            <figcaption className="font-roboto text-base text-center">
+            <figcaption className={style.figcaption}>
               {t('TextHelpOne')}
             </figcaption>
           </figure>
 
-          <figure className="w-[275px] h-[132px] flex flex-col justify-center items-center gap-4 px-4">
-            <Image src={tankImg} alt="insta" width={100} height={60} priority />
-            <figcaption className="font-roboto text-base  text-center">
+          <figure className={style.figure}>
+            <Image src={tankImg} alt="tank" width={100} height={60} priority />
+            <figcaption className={style.figcaption}>
               {t('TextHelpTwo')}
             </figcaption>
           </figure>
-          <figure className="w-[275px] h-[132px] flex flex-col justify-center items-center gap-4">
+          <figure className={style.figure}>
             <Image
               src={puzzleImg}
-              alt="insta"
+              alt="puzzle"
               width={60}
               height={60}
               priority
             />
-            <figcaption className="font-roboto text-base pb-2 text-center">
+            <figcaption className={style.figcaption}>
               {t('TextHelpThree')}
             </figcaption>
           </figure>
-          <figure className="w-[275px] h-[132px] flex flex-col justify-center items-center gap-4">
+          <figure className={style.figure}>
             <Image
               src={disabilityImg}
-              alt="insta"
+              alt="disability"
               width={54}
               height={60}
               priority
             />
-            <figcaption className="font-roboto text-base pb-2 text-center">
+            <figcaption className={style.figcaption}>
               {t('TextHelpFour')}
             </figcaption>
           </figure>
