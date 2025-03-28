d8dbe6b60c2f95fd99ed9d8b376b7970a3cd038e
Yana Diachok <annetart1994@gmail.com>
2025-03-28 07:47:15 +0200

    fix: fix db

--- hippotherapy/src/db/news.ts
+++ hippotherapy/src/db/news.ts
@@ -1,6 +1,7 @@
 import { IDataItem } from '@/types/interfaces';
 import photoOne from '../assets/gallery/img02.png';
 import photoTwo from '../assets/gallery/img03.png';
+
 export const news: IDataItem[] = [
   { photo: photoOne, title: 'NewsTitleOne', subTitle: 'NewsSubTitleOne' },
   { photo: photoTwo, title: 'NewsTitleTwo', subTitle: 'NewsSubTitleTwo' },
--- hippotherapy/src/db/partners.ts
+++ hippotherapy/src/db/partners.ts
@@ -4,6 +4,7 @@ import photoTwo from '../assets/svg/partner_2.svg';
 import photoThree from '../assets/svg/partner_3.svg';
 import photoFour from '../assets/svg/partner_4.svg';
 import photoFive from '../assets/svg/partner_5.svg';
+
 export const partners: Pick<IDataItem, 'photo'>[] = [
   { photo: photoOne },
   { photo: photoTwo },
